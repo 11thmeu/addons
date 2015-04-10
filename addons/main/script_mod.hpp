f05d4aa057e516408277ec91bc3519a7cf6fae6b
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 15:51:10 +0200

    Changed Mod Prefix from meu to MEU

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -1,7 +1,7 @@
 // COMPONENT should be defined in the script_component.hpp and included BEFORE this hpp
 
 #define MAINPREFIX z
-#define PREFIX meu
+#define PREFIX MEU
 
 #define MAJOR 2
 #define MINOR 0
