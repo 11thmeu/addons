5c1d1ed91efb9e0b336c350a5cf61371056b869f
Kavinsky <nmunozfernandez@gmail.com>
2015-04-11 00:50:41 +0200

    Added More macros

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,7 +1,39 @@
 #include "\x\cba\addons\main\script_macros_common.hpp"
 #include "\x\cba\addons\xeh\script_xeh.hpp"
 
+#define EGVAR(module,var) TRIPLES(PREFIX,module,var)
+#define QEGVAR(module,var) QUOTE(EGVAR(module,var))
+
 #define DFUNC(var1) TRIPLES(ADDON,fnc,var1)
+#define DEFUNC(var1,var2) TRIPLES(DOUBLES(PREFIX,var1),fnc,var2)
+
+#define QFUNC(var1) QUOTE(DFUNC(var1))
+#define QEFUNC(var1,var2) QUOTE(DEFUNC(var1,var2))
+
+#define PATHTOEF(var1,var2) PATHTOF_SYS(PREFIX,var1,var2)
+
+
+#define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
+#define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
+#define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
+
+#define GETVAR(var1,var2,var3) var1 GETVAR_SYS(var2,var3)
+#define GETMVAR(var1,var2) missionNamespace GETVAR_SYS(var1,var2)
+#define GETUVAR(var1,var2) uiNamespace GETVAR_SYS(var1,var2)
+#define GETPRVAR(var1,var2) profileNamespace GETVAR_SYS(var1,var2)
+#define GETPAVAR(var1,var2) parsingNamespace GETVAR_SYS(var1,var2)
+
+#define SETVAR(var1,var2,var3) var1 SETVAR_SYS(var2,var3)
+#define SETPVAR(var1,var2,var3) var1 SETPVAR_SYS(var2,var3)
+#define SETMVAR(var1,var2) missionNamespace SETVAR_SYS(var1,var2)
+#define SETUVAR(var1,var2) uiNamespace SETVAR_SYS(var1,var2)
+#define SETPRVAR(var1,var2) profileNamespace SETVAR_SYS(var1,var2)
+#define SETPAVAR(var1,var2) parsingNamespace SETVAR_SYS(var1,var2)
+
+#define GETGVAR(var1,var2) GETMVAR(GVAR(var1),var2)
+#define GETEGVAR(var1,var2,var3) GETMVAR(EGVAR(var1,var2),var3)
+
+#define ARR_SELECT(ARRAY,INDEX,DEFAULT) if (count ARRAY > INDEX) then {ARRAY select INDEX} else {DEFAULT}
 
 #define MACRO_ADDWEAPON(WEAPON,COUNT) class _xx_##WEAPON { \
   weapon = #WEAPON; \
