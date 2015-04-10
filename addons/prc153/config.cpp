5c116c1a736d4b862629506f5f9c299458c3c6bc
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 18:38:11 +0200

    added initial of prc153

---
+++ addons/prc153/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cba_main", "extended_eventhandlers", "CBA_UI", "CBA_XEH", "CBA_XEH_A3",
+            "ace_common",
+            "task_force_radio", "task_force_radio_items",
+            "meu_main", "meu_radios"
+        };
+        author[] = {"11thMEU Dev Team"};
+        authorUrl = "http://11thmeu.es";
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/prc153/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT PRC153
+#include "\z\meu\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+
+#ifdef DEBUG_ENABLED_PRC153
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_PRC153
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_PRC153
+#endif
+
+#include "\z\meu\addons\main\script_macros.hpp"
