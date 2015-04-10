3d9dfc767298dfdeb631c6a6594cc6281ea6ae82
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 14:40:28 +0200

    changed script_component debug in ex2mod0

--- addons/ex2mod0/script_component.hpp
+++ addons/ex2mod0/script_component.hpp
@@ -3,12 +3,12 @@
 
 // #define DEBUG_MODE_FULL
 
-#ifdef DEBUG_ENABLED_CORE
+#ifdef DEBUG_ENABLED_EX2MOD0
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_CORE
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#ifdef DEBUG_SETTINGS_EX2MOD0
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_EX2MOD0
 #endif
 
 #include "\z\meu\addons\main\script_macros.hpp"
