0bbbccba976d923f3704e5cfe660a9443abb8a42
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 02:28:35 +0200

    Fixed spacing issue

--- addons/ex2mod0/config.cpp
+++ addons/ex2mod0/config.cpp
@@ -9,7 +9,7 @@ class CfgPatches {
             "a3_weapons_f",
             "a3_weapons_f_beta",
             "a3_weapons_f_epa",            
-        	"cba_main", "extended_eventhandlers", "CBA_UI", "CBA_XEH", "CBA_XEH_A3"
+            "cba_main", "extended_eventhandlers", "CBA_UI", "CBA_XEH", "CBA_XEH_A3"
         };
         author[] = {"11thMEU Dev Team"};
         authorUrl = "http://11thmeu.es";
