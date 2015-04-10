17e9201a613d914dcfcf01cf42a5a0f4553f4cbb
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 17:55:41 +0200

    fixed naming of MEU Modules :D

--- addons/main/CfgModuleCategories.hpp
+++ addons/main/CfgModuleCategories.hpp
@@ -1,6 +1,6 @@
 class CfgFactionClasses {
     class NO_CATEGORY;
-    class ACE: NO_CATEGORY {
+    class MEU_Modules: NO_CATEGORY {
         displayName = "11th MEU";
         priority = 2;
         side = 7;
