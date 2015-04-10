b952eda0f9e1b88e80a27ddd748598570b218f6d
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 16:21:10 +0200

    corrected name on script_component of map

--- addons/map/script_component.hpp
+++ addons/map/script_component.hpp
@@ -3,12 +3,12 @@
 
 // #define DEBUG_MODE_FULL
 
-#ifdef DEBUG_ENABLED_CORE
+#ifdef DEBUG_ENABLED_MAP
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_CORE
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#ifdef DEBUG_SETTINGS_MAP
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MAP
 #endif
 
 #include "\z\meu\addons\main\script_macros.hpp"
