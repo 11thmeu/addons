233f019326ebe2bb5d5b4a794cbe2e21640bf7dc
Kavinsky <nmunozfernandez@gmail.com>
2015-04-10 16:20:14 +0200

    updated script_component in functions

--- addons/map/functions/script_component.hpp
+++ addons/map/functions/script_component.hpp
@@ -1,14 +1,1 @@
-#define COMPONENT map
-#include "\z\meu\addons\main\script_mod.hpp"
-
-// #define DEBUG_MODE_FULL
-
-#ifdef DEBUG_ENABLED_CORE
-    #define DEBUG_MODE_FULL
-#endif
-
-#ifdef DEBUG_SETTINGS_CORE
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
-#endif
-
-#include "\z\meu\addons\main\script_macros.hpp"
+#include "\z\meu\addons\map\script_component.hpp"
