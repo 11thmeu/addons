d59f9f8be9ff38a70e9225a352ebdf0732c7d28a
Kavinsky <nmunozfernandez@gmail.com>
2015-05-19 01:22:42 +0200

    updated macros

--- addons/magazines/macros.hpp
+++ addons/magazines/macros.hpp
@@ -5,8 +5,20 @@
 #define MAGAZINE_GROUP_MP5_9MM magazineGroup[] = {"MEU_MP5_9mm"}
 #define MAGAZINE_GROUP_MK11 magazineGroup[] = {"MEU_MK11"}
 
+#define MAGAZINE_LIST_M203_GL "1Rnd_HE_Grenade_shell", "UGL_FlareWhite_F", "UGL_FlareGreen_F", "UGL_FlareRed_F", "UGL_FlareYellow_F", "UGL_FlareCIR_F", \
+                    "1Rnd_Smoke_Grenade_shell", "1Rnd_SmokeRed_Grenade_shell", "1Rnd_SmokeGreen_Grenade_shell", "1Rnd_SmokeYellow_Grenade_shell", \
+                    "1Rnd_SmokePurple_Grenade_shell", "1Rnd_SmokeBlue_Grenade_shell", "1Rnd_SmokeOrange_Grenade_shell", \
+                    "rhs_mag_M441_HE", "rhs_mag_M433_HEDP", "rhs_mag_M4009", "rhs_mag_m576", "rhs_mag_M585_white", "rhs_mag_M661_green", "rhs_mag_M662_red", \
+                    "rhs_mag_M713_red", "rhs_mag_M714_white", "rhs_mag_M715_green", "rhs_mag_M716_yellow"
 
+
+// Magazines for 5.56x45mm NATO
 #define MAGAZINE_LIST_STANAG4179_RIFLE "MEU_30Rnd_STANAG4179_M855A1", "MEU_30Rnd_STANAG4179_M856A1", "MEU_30Rnd_STANAG4179_Mk262Mod1", "MEU_20Rnd_STANAG4179_Mk262Mod1", \
 "MEU_100Rnd_BetaCMag"
 
 #define MAGAZINE_LIST_STANAG4179_M249 "MEU_200Rnd_M249_M855A1", "MEU_200Rnd_M249_M856A1", "MEU_100Rnd_M249_M855A1", "MEU_100Rnd_M249_M856A1"
+
+// Magazines for .300 AAC weapons
+#define MAGAZINE_LIST_STANAG300 "MEU_29_STANAG300_OTM", "MEU_29_STANAG300_OTM_S", \
+                      "29rnd_300BLK_STANAG", "29rnd_300BLK_STANAG_T", "29rnd_300BLK_STANAG_S", \
+                      "RH_30Rnd_762x35_FMJ", "RH_30Rnd_762x35_Match", "RH_30Rnd_762x35_MSB"
