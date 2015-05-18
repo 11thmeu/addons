51f3961d17997490b2ba8c3d8aa2c8574a6f9459
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 12:36:51 +0200

    Added PRC117G Coy temporary radio

---
+++ addons/radios/CfgVehicles.hpp
@@ -0,0 +1,8 @@
+class CfgVehicles {
+    class MEU_PRC117G_NicecommII_Coy: tf_rt1523g {
+        displayName = "AN/PRC-117G Coyote";
+        descriptionShort = "AN/PRC-117G Coyote";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {"\task_force_radio_items\models\data\clf_nicecomm2_coyote_co.paa"};
+    };
+};
--- addons/radios/config.cpp
+++ addons/radios/config.cpp
@@ -18,3 +18,4 @@ class CfgPatches {
 };
 
 #include "CfgFunctions.hpp"
+#include "CfgVehicles.hpp"
