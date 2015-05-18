5347e0b9d6e4b6cd261746aa0dc702677a9568de
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 04:12:39 +0200

    forgot to add ammoboxes config

---
+++ addons/ammoboxes/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"meu_main"};
+        author[] = {"11thMEU Dev Team"};
+        authorUrl = "http://11thmeu.es";
+        versionDesc = "11thMEU Addons";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
