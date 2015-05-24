3ae240f465ed2153b5f94dd633f2ace0b11f8a08
Kavinsky <nmunozfernandez@gmail.com>
2015-05-24 19:39:47 +0200

    fixed error on non supported 45acp

--- addons/ammunition/CfgAmmo.hpp
+++ addons/ammunition/CfgAmmo.hpp
@@ -1,7 +1,6 @@
 class CfgAmmo
 {
     #include "CfgAmmo\9x19.hpp"
-    #include "CfgAmmo\45acp.hpp"
     #include "CfgAmmo\556x45.hpp"
     #include "CfgAmmo\300BO.hpp"
     #include "CfgAmmo\762x51.hpp"
