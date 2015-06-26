753d343a521f3cc7917f52b7de79edbf0c412b9c
Kavinsky <nmunozfernandez@gmail.com>
2015-06-27 01:21:42 +0200

    syntax fix on CfgEventHandlers on hearing_protectors

--- addons/hearing_protectors/CfgEventHandlers.hpp
+++ addons/hearing_protectors/CfgEventHandlers.hpp
@@ -4,10 +4,8 @@ class Extended_PreInit_EventHandlers {
     };
 };
 
-class Extended_PostInit_EventHandlers
-{
-    class ADDON
-    {
+class Extended_PostInit_EventHandlers {
+    class ADDON {
         clientInit = QUOTE( call COMPILE_FILE(XEH_clientInit) );
     };
 };
