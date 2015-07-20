f9c4ff5bd4812de71460945b8f18c4b41ce9a95f
Kavinsky <nmunozfernandez@gmail.com>
2015-07-20 15:53:00 +0200

    added presetTemplate

---
+++ extras/presetTemplate.hpp
@@ -0,0 +1,42 @@
+class MEU_Preset_CatName {
+    displayName = "blabla";
+    picture = ""; // optional
+
+    // list of presets
+    class Presets {
+        class presetNameInternal {
+            displayName = "";
+            scope = 2;
+
+            conditions[] = {"isGroupLeader"};
+            radioPreset = "";
+
+            // personalization for multiple selects.
+            uniform[] = {};
+            vest[] = {};
+            backpack[] = {}; // empty for nothing
+            headgear[] = {};
+            glasses[] = {};
+
+            // content of containers
+            class UniformContainer[] = {
+
+            };
+
+            class VestContainer[] = {
+
+            };
+
+            class BackpackContainer[] = {
+
+            };
+
+            class linkedItems[] = {};
+
+            primaryWeapon[] = {};
+            secondaryWeapon[] = {};
+            sideWeapon[] = {};
+
+        };
+    };
+};
